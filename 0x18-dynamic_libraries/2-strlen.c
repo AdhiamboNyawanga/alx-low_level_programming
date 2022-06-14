704deff86fe1db3ff6efed31806a45bdeec3c169
Nyabonyo <nyawangasyprose@gmail.com>
2022-06-14 05:09:22 +0300

    my commit

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * _strlen -  returns the length of a string
+ *
+ * @s: the string to check
+ *  * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+return (i);
+}
